b8218574cbd668ba30e06050dab2857759dd1cf5
mintak-Macbook <mintak21@gmail.com>
2021-12-24 02:06:06 +0900

    add binding

--- projects/mintak/main.tf
+++ projects/mintak/main.tf
@@ -1,8 +1,15 @@
 module "mintak" {
-  source = "github.com/mintak21/terraform-modules.git//gcp/project?ref=v0.1.0"
+  source = "github.com/mintak21/terraform-modules.git//gcp/project?ref=v0.3.3"
 
-  name               = "mintak"
-  billing_account_id = var.billing_account_id
+  name                   = "mintak"
+  authorize_github_owner = "mintak21"
+  billing_account_id     = var.billing_account_id
+}
+
+resource "google_project_iam_member" "github_actions" {
+  project = module.mintak.project_id
+  role    = "roles/editor" // FIXME 本来は個別に権限をふるべき
+  member  = "serviceAccount:${module.mintak.service_account_github_actions_email}"
 }
 
 variable "billing_account_id" {
@@ -11,5 +18,13 @@ variable "billing_account_id" {
 }
 
 output "project_id" {
-  value = module.mintak.id
+  value = module.mintak.project_id
+}
+
+output "service_account_mintak_github_email" {
+  value = module.mintak.service_account_github_actions_email
+}
+
+output "iam_workload_identity_pool_provider_mintak_name" {
+  value = module.mintak.google_iam_workload_identity_pool_provider_github_name
 }
